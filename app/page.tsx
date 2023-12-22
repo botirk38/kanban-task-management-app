69c4b866be5b5795855593d87f84a4b24a5e657f
Botir <btrghstk@gmail.com>
2023-12-22 15:46:21 +0200

    Fix UserButton afterSignOutUrl

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs"
 const Page = () => {
     return (
         <div>
-            <UserButton afterSignOutUrl="/"/>
+            <UserButton afterSignOutUrl="/" />
         </div>
     )
 }
