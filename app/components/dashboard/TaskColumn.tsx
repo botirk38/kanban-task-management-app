d33f96e6f26ae2cdb1b8c7f23324eea550f73f54
Botir <btrghstk@gmail.com>
2024-01-01 17:39:34 +0200

    commit task undefined bug fix

--- app/components/dashboard/TaskColumn.tsx
+++ app/components/dashboard/TaskColumn.tsx
@@ -17,10 +17,10 @@ const TaskColumn: React.FC<TaskColumnProps> = ({column, openTask}) => {
                 </span>
             )}
             <h2 className='font-bold tracking-widest uppercase text-blue-gray-light'>
-                {column.name} ({column.tasks.length})
+                {column.name} ({column.tasks?.length})
             </h2>
         </span>
-        {column.tasks.map((task, taskIndex) => (
+        {column.tasks?.map((task, taskIndex) => (
             <button onClick={() => openTask(task)} key={taskIndex} className='bg-white dark:bg-blue-gray w-[22rem] shadow-xl rounded-lg p-4 flex flex-col justify-start items-start text-start gap-1'>
                 <h3 className='text-blue-dark font-bold text-lg dark:text-white'>{task.title}</h3>
                 <p className='text-blue-gray-light font-bold text-sm dark:text-blue-grayish'>{task.subtasks?.filter(st => st.isCompleted === false).length} of {task.subtasks?.length} subtasks</p>
